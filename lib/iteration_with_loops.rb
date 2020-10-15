03161df0c18ac91fa092c18ee292b80a8b0b7f4e
Dena Neuman <denaweiss5@gmail.com>
2020-10-15 01:27:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,17 +1,17 @@
 
 def join_nested_strings(src)
 
-join_nested_strings= []
+outer_results= []
 row_index= 0
   while row_index<src.count do
     element_index=0
     while element_index< src[row_index].count do
       if src[row_index][element_index].class== String
-        join_nested_strings<<src[row_index][element_index]
+        outer_results<<src[row_index][element_index]
       end
     element_index+=1
     end
   row_index+=1
   end
-  join_nested_strings(mixed_data_1)
+  outer_results(mixed_data_1)
 end
